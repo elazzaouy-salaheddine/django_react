12c4782200f358b88ad2d54a53b845760abab002
salaheddine el azzaouy <salaheddine.elazzaouy@outlook.fr>
2021-03-21 17:14:45 +0100

    sfsf

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'b^*htb0g77h&&sk&%4i*pc1&^d)i#%kbx-mhq5^-6*oz*w$qbq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','gtedjangoapi.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','gtedjangoapi.herokuapp.com','localhost:5000']
 
 
 # Application definition
