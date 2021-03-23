3c12151dbba36131ab3d9b8262413b624aadd670
elazzaouy-salaheddine <salaheddine.elazzaouy@outlook.fr>
2021-03-23 17:26:54 +0100

    add static files in templates react

--- frontend/src/layout/Footer.js
+++ frontend/src/layout/Footer.js
@@ -3,9 +3,15 @@ import React, { Component } from 'react'
 export default class Footer extends Component {
     render() {
         return (
-            <div>
-                Footer
-            </div>
+            <footer className="footer footer-static footer-light ">
+                <p className="clearfix mb-0">
+                    <span className="float-left d-inline-block">2020 &copy; PIXINVENT</span>
+                    <span className="float-right d-sm-inline-block d-none">Crafted with
+                    <i className="bx bxs-heart pink mx-50 font-small-3"></i>by
+                    <a className="text-uppercase" href="https://1.envato.market/pixinvent_portfolio" target="_blank">Pixinvent</a></span>
+                <button className="btn btn-primary btn-icon scroll-top" type="button"><i className="bx bx-up-arrow-alt"></i></button>
+                </p>
+            </footer>
         )
     }
 }
---
+++ frontend/src/layout/Navbarbottom.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react'
+
+export default class Navbarbottom extends Component {
+    render() {
+        return (
+            <div className="header-navbar navbar-expand-sm navbar navbar-horizontal navbar-fixed navbar-light navbar-without-dd-arrow" role="navigation" data-menu="menu-wrapper">
+            <div className="navbar-header d-xl-none d-block">
+                <ul className="nav navbar-nav flex-row">
+                    <li className="nav-item mr-auto"><a className="navbar-brand" href="index.html">
+                            <div className="brand-logo">
+                                <img className="logo" src={"static/frontend/app-assets/images/logo/logo.png"} />
+                            </div>
+                            <h2 className="brand-text mb-0">Frest</h2>
+                        </a></li>
+                    <li className="nav-item nav-toggle"><a className="nav-link modern-nav-toggle pr-0" data-toggle="collapse"><i className="bx bx-x d-block d-xl-none font-medium-4 primary toggle-icon"></i></a></li>
+                </ul>
+            </div>
+            <div className="shadow-bottom"></div>
+      
+            <div className="navbar-container main-menu-content" data-menu="menu-container">
+              
+                <ul className="nav navbar-nav" id="main-menu-navigation" data-menu="menu-navigation" data-icon-style="filled">
+                    <li>
+                        <a href="index.html" className='pr-2'>Dashboard</a>
+
+                    </li>
+                    <li>
+                        <a href="index.html" className='pr-2'>Gte</a>
+
+                    </li>
+                    <li>
+                        <a href="index.html" className='pr-2 '>Nov Energie </a>
+
+                    </li>
+                    <li>
+                        <a href="index.html" className='pr-2'>Clients</a>
+
+                    </li>
+                    <li>
+                        <a href="index.html" className='pr-2'>Lots</a>
+
+                    </li>
+
+                    <li>
+                        <a href="index.html" className='pr-2'>Architect</a>
+
+                    </li>
+                    <li>
+                        <a href="index.html" className='pr-2'>Opc</a>
+
+                    </li>
+
+                </ul>
+            </div>
+        
+        </div>
+        )
+    }
+}
